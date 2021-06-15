4dfc7ac0f9a32ebe9d2cef61de2ce2ed037c300e
J <gapspace@engineer.com>
2021-06-15 22:34:12 +0100

    The basics here are complete:
    Kafka stream: Set
    Database: Set
    Rest(Post): Set
    
    We could tidy things up with some OOD and perhaps satisfy more requirements if there is time.

--- src/main/java/com/wefox/project/ProjectApplication.java
+++ src/main/java/com/wefox/project/ProjectApplication.java
@@ -9,12 +9,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.*;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -77,6 +80,38 @@ public class ProjectApplication {
 
 		// Step 2: Post the log data
 
+		// request url
+		String url = "http://localhost:9000/payment";
+
+		// create an instance of RestTemplate
+		RestTemplate restTemplate = new RestTemplate();
+
+		// create headers
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+		// request body parameters
+		Map<String, Object> map = new HashMap<>();
+		map.put("payment_id", "John Doe");
+		map.put("account_id", "Java Developer");
+		map.put("payment_type","test");
+		map.put("credit_card","test");
+		map.put("amount","test");
+
+		// build the request
+		HttpEntity<Map<String, Object>> entity = new HttpEntity<>(map, headers);
+
+		// send POST request
+		ResponseEntity<String> response = restTemplate.postForEntity(url, entity, String.class);
+
+		// check response
+		if (response.getStatusCode() == HttpStatus.OK) {
+			System.out.println("Payment Request Successful");
+		} else {
+			System.out.println("Payment Request Failed");
+			System.out.println(response.getStatusCode());
+		}
 
 	}
 
